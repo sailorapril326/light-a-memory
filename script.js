a4fd75c35ed280fa137eb1cb995dd221d3317819
sailorapril326 <sailor.april326@gmail.com>
2026-07-09 19:25:00 +0300

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 console.log("Light a Memory Loaded");
+const homeScreen = document.getElementById("homeScreen");
+const memoryScreen = document.getElementById("memoryScreen");
+
+const startButton = document.getElementById("startButton");
+const backButton = document.querySelector(".back-button");
+
 startButton.addEventListener("click", () => {
-    // מה יקרה כשהמשתמש ילחץ
+    homeScreen.classList.add("hidden");
+    memoryScreen.classList.remove("hidden");
+});
+
+backButton.addEventListener("click", () => {
+    memoryScreen.classList.add("hidden");
+    homeScreen.classList.remove("hidden");
 });
